9def2bdbc6f11a61084b1dc631ea9437af0e9c28
Olliemint <furywrld5@gmail.com>
2022-11-03 15:37:56 +0300

    feat: added button functionality using useState hook

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,7 +1,20 @@
-import React from "react";
+import React,{useState} from 'react'
 
-function Toggle() {
-  return <button>OFF</button>;
+const Toggle = () => {
+  const [isON, setIsOn] = useState(false)
+  const color = isON ? 'green' : 'red'
+  
+  function handleClick() {
+    setIsOn((isON)=> !isON)
+  }
+
+  return (
+    <div>
+      <h1>Toggle me </h1>
+
+      <button style={{background: color}} onClick={handleClick} type="submit">{ isON ? 'ON' : 'OFF' }</button>
+    </div>
+  )
 }
 
-export default Toggle;
+export default Toggle
